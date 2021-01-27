99b247a1c79ad5de5c062195f34464c0887d6813
naveena3993 <54440527+naveena3993@users.noreply.github.com>
2021-01-27 15:44:07 +0530

    Update cloudexample.tf

--- cloudexample.tf
+++ cloudexample.tf
@@ -1,15 +1,19 @@
 provider "azurerm" {
   # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
 
-  subscription_id = "bd65f624-8c90-4842-ad3b-1ec18d74a32d"
-  client_id       = "075ebc65-ea69-49a8-9c12-1cb84cdc53f4"
-  client_secret   = "rk9pp97s2~J.UpSgY9o-tB~04qjjWMDUaz"
-  tenant_id       = "ff7fcf1f-c9ab-464f-b2bd-a9c773e50ab9"
+  subscription_id = "71ce6b58-b10d-4f36-8f98-b1728111b315"
+  client_id       = "de02f09c-5666-4b88-9a15-38a37a098704"
+  client_secret   = "5UX~S9.qv-BT.x3W~KX6Pg80Zdxd3_pU2W"
+  tenant_id       = "4536e94f-051b-4555-893d-1529df2d08eb"
 
   features {}
 }
 
 resource "azurerm_resource_group" "example" {
   name     = "azurermforme01"
+  location = "westus"
+}
+resource "azurerm_storage_account" "example1" {
+  name     = "strazurermforme01"
   location = "westus"
 }
