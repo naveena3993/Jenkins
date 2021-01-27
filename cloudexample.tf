6a427ddc3dcb827555c897121a4047efe4876d15
naveena3993 <54440527+naveena3993@users.noreply.github.com>
2021-01-27 15:47:37 +0530

    Update cloudexample.tf

--- cloudexample.tf
+++ cloudexample.tf
@@ -13,7 +13,7 @@ resource "azurerm_resource_group" "example" {
   name     = "azurermforme01"
   location = "westus"
 }
-resource "azurerm_storage_account" "example" {
+resource "azurerm_storage_account" "examplee" {
   name                     = "storageaccountname"
   resource_group_name      = azurerm_resource_group.example.name
   location                 = azurerm_resource_group.example.location
