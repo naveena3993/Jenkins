f2ff80ee54595a0a2876ed02d0de23c663c75d42
naveena3993 <54440527+naveena3993@users.noreply.github.com>
2021-01-28 15:57:42 +0530

    Update cloudexample.tf

--- cloudexample.tf
+++ cloudexample.tf
@@ -1,11 +1,6 @@
 provider "azurerm" {
   # Whilst version is optional, we /strongly recommend/ using it to pin the version of the Provider being used
 
-  subscription_id = "71ce6b58-b10d-4f36-8f98-b1728111b315"
-  client_id       = "de02f09c-5666-4b88-9a15-38a37a098704"
-  client_secret   = "5UX~S9.qv-BT.x3W~KX6Pg80Zdxd3_pU2W"
-  tenant_id       = "4536e94f-051b-4555-893d-1529df2d08eb"
-
   features {}
 }
 resource "azurerm_resource_group" "example" {
