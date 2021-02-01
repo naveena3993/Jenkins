5581fa1f9cb87f06ccd9361c4af420d6b77e894a
naveena3993 <54440527+naveena3993@users.noreply.github.com>
2021-02-01 18:58:13 +0530

    Update cloudexample.tf

--- cloudexample.tf
+++ cloudexample.tf
@@ -19,7 +19,7 @@ resource "azurerm_resource_group" "example" {
   location = "westus"
 }
 resource "azurerm_key_vault" "example" {
-  name                        = "examplekeyvault0101"
+  name                        = "keyvault000110"
   location                    = azurerm_resource_group.example.location
   resource_group_name         = azurerm_resource_group.example.name
   enabled_for_disk_encryption = true
