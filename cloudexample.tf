795d53bea6f1086c0b8c89d5c7bed0b00bda6cd4
naveena3993 <54440527+naveena3993@users.noreply.github.com>
2021-02-01 17:55:34 +0530

    Update cloudexample.tf

--- cloudexample.tf
+++ cloudexample.tf
@@ -21,7 +21,7 @@ resource "azurerm_key_vault" "example" {
   resource_group_name         = azurerm_resource_group.example.name
   enabled_for_disk_encryption = true
   soft_delete_retention_days  = 7
-  tenant_id                   = var.tenant.id
+  tenant_id                   = var.tenant_id
   purge_protection_enabled    = false
 
   sku_name = "standard"
