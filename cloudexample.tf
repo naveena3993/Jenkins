fa60671aae5bdf3b0841270edfc5604cc747f711
naveena3993 <54440527+naveena3993@users.noreply.github.com>
2021-02-02 18:00:43 +0530

    Update cloudexample.tf

--- cloudexample.tf
+++ cloudexample.tf
@@ -11,7 +11,17 @@ provider "azurerm" {
     }
   }
 }
+terraform {
+  backend "azurerm" {
+    storage_account_name = "abcd1234"
+    container_name       = "tfstate"
+    key                  = "prod.terraform.tfstate"
 
+    # rather than defining this inline, the Access Key can also be sourced
+    # from an Environment Variable - more information is available below.
+    access_key = var.accesskey
+  }
+}
 
 
 resource "azurerm_resource_group" "example" {
