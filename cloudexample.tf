967ff9756cb6fdc161ad1011f8ad6d682940a411
naveena3993 <54440527+naveena3993@users.noreply.github.com>
2021-02-03 11:57:30 +0530

    Update cloudexample.tf

--- cloudexample.tf
+++ cloudexample.tf
@@ -6,12 +6,6 @@ terraform {
   }
 }
 provider "azurerm" {
-    version         =   ">= 2.26"
-    client_id       =   var.client_id
-    client_secret   =   var.client_secret
-    subscription_id =   var.subscription_id
-    tenant_id       =   var.tenant_id
-    
     features {
     key_vault {
       purge_soft_delete_on_destroy = true
